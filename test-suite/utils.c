bc9b76244d5e996bb54a363452228f07baf9cc11
Pablo Fernandez <pablo@littleq.net>
2001-10-06 19:22:30 +0000

    Toolbar widget work

--- test-suite/utils.c
+++ test-suite/utils.c
@@ -15,46 +15,20 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-#include <stdio.h>
-#include <glib.h>
-
-#include <libcronosII/mailbox.h>
-#include <libcronosII/pop3.h>
-#include <libcronosII/smtp.h>
-#include <libcronosII/account.h>
-#include <libcronosII/error.h>
 #include <libcronosII/utils.h>
+
+#define STRING "Pablo Ferná(ndez) Navarro <cronosII@users.sourceforge.net> (Yahoo!)"
+#define ENC1 '('
+#define ENC2 ')'
 
 gint
 main (gint argc, gchar **argv)
 {
-	C2Mailbox *inbox, *head = NULL;
-	C2POP3 	*pop3 = NULL;
-	gint	flags =	0;
-	C2Account *act = NULL;
+	gchar *string;
 
-	gtk_init (&argc, &argv);
-
-	flags = C2_POP3_DO_KEEP_COPY;
-
-	inbox = c2_mailbox_new (&head, "Inbox2", "0", C2_MAILBOX_CRONOSII, 0, 0);
-	pop3 = c2_pop3_new(g_strdup (argv[1]), 110, g_strdup (argv[2]), g_strdup (argv[3]), FALSE);
-	act  = c2_account_new(C2_ACCOUNT_POP3, "Me", "root@localhost");
-
-	c2_pop3_set_flags(pop3,flags);
-
-//	if(c2_net_object_run (C2_NET_OBJECT (pop3)) < 0)
-//	{
-//		
-//	}
-
-	c2_pop3_fetchmail(pop3, act, inbox);
-
-
-	//c2_net_object_disconnect_with_error (C2_NET_OBJECT (pop3));
-
-
-
+	printf ("'%s', '%c', '%c' = \n", STRING, ENC1, ENC2);
+	string = c2_str_get_enclosed_text_backward (STRING, ENC1, ENC2, 0);
+	printf ("%s\n", string);
+	
 	return 0;
 }
-
